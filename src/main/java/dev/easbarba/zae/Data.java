a8969a59b345f77520438d2028891a86c9d7773b
easbarba <easbarba@outlook.com>
2022-12-28 23:28:02 -0300

    data: check if user set xdg config home and return it

--- src/main/java/dev/easbarba/zae/Data.java
+++ src/main/java/dev/easbarba/zae/Data.java
@@ -6,17 +6,28 @@ import java.nio.file.Paths;
 
 public class Data {
     private final Path userHome = Paths.get(System.getProperty("user.home"));
+
+    // Default configuration directory.
     private String ConfigDir = Paths.get(userHome.toString(), ".config", "zae").toString();
+
+    // User set XDG_CONFIG_HOME value
+    private String XDGConfigDir = Paths.get(System.getenv("XDG_CONFIG_HOME"), "zae").toString();
 
     public Path getHome() {
         return userHome;
     }
 
+    // Return XDG_CONFIG_HOME value or default configuration directory.
     public String getConfigDir() {
+        if (System.getenv("XDG_CONFIG_HOME") != null) {
+            return XDGConfigDir;
+        }
+
         return ConfigDir;
     }
 
-    public String[] listOfConfigFiles() {
+    // List of configuration basename files found.
+    public String[] ConfigFiles() {
         return new File(ConfigDir).list();
     }
 }
